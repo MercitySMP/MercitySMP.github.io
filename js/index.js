9ad73241ded2cf744d693ebef0b6939b5237fca5
chill31 <89910163+chill31@users.noreply.github.com>
2022-09-04 13:14:32 +0530

    Mercity Redesign
    redesigned mercity smp website with a new accent color and a lot more features.

---
+++ js/index.js
@@ -0,0 +1,178 @@
+import Swiper from '../static/slider/script.js';
+
+const sectionHeaders = document.querySelectorAll(".section__header");
+sectionHeaders.forEach(header => {
+  header.addEventListener("click", (e) => {
+    window.location.href = `#${e.target.parentElement.id}`
+  })
+});
+
+const copyIPBtn = document.querySelector(".btn__copyIP");
+copyIPBtn.addEventListener("click", () => {
+  copyText("join.mercitysmp.cf");
+  notify("Copied server IP", "success");
+});
+
+const swiper = new Swiper(".swiper1", {
+  effect: "cards",
+  rewind: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const staffMembers = [
+  {
+    "name": "Grailmit",
+    "tag": "3114",
+    "full_name": "Grailmit#3114",
+    "avatar": "/assets/img/admin/grailmit.png",
+    "role": "Owner"
+  },
+  {
+    "name": "Optid2",
+    "tag": "7706",
+    "full_name": "Optid2#7706",
+    "avatar": "/assets/img/admin/optid2.png",
+    "role": "Owner"
+  },
+  {
+    "name": "Satindar",
+    "tag": "2927",
+    "full_name": "Satindar#2927",
+    "avatar": "/assets/img/admin/satindar.png",
+    "role": "Co-Owner"
+  },
+  {
+    "name": "TitanGamerOP",
+    "tag": "3937",
+    "full_name": "TitanGamerOP#3937",
+    "avatar": "/assets/img/admin/titangamerOP.png",
+    "role": "Admin"
+  },
+  {
+    "name": "Chill31",
+    "tag": "6466",
+    "full_name": "Chill31#6466",
+    "avatar": "/assets/img/admin/Chill31.png",
+    "role": "Website Developer"
+  }
+]
+
+staffMembers.forEach(member => {
+
+  const createdCard = document.createElement("div");
+  createdCard.classList.add("swiper-slide");
+
+  createdCard.innerHTML = `
+  
+<img src="${member.avatar}" alt="${member.name}" class="user-avatar" />
+
+<span class="user-header">
+
+  <span class="user-name">${member.name}</span><span class="user-tag">#${member.tag}</span>
+
+</span>
+
+<span class="user-role">${member.role}</span>
+
+  `;
+
+  swiper.appendSlide(createdCard);
+  swiper.slideTo(0);
+
+});
+
+const swiperHeaders = document.querySelectorAll(".user-header");
+
+for (let i = 0; i < swiperHeaders.length; i++) {
+
+  swiperHeaders[i].addEventListener("click", () => {
+    copyText(staffMembers[i].full_name);
+    notify(`Copied username of ${staffMembers[i].name}`, "info");
+  });
+
+}
+
+const swiper2 = new Swiper(".swiper2", {
+  effect: "cards",
+  rewind: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+/* ACCORDION CODE */
+
+const minusSign = "bi-dash";
+const plusSign = "bi-plus";
+
+let toggles = document.querySelectorAll(".toggle");
+let contentDiv = document.querySelectorAll(".content");
+let icons = document.querySelectorAll(".icon");
+
+for (let i = 0; i < toggles.length; i++) {
+
+  toggles[i].addEventListener("click", () => {
+
+    if (parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight) {
+
+      contentDiv.forEach(div => {
+        div.style.height = "0px";
+      })
+
+      contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
+      icons[i].classList.remove(plusSign);
+      icons[i].classList.add(minusSign);
+
+    } else {
+
+      contentDiv[i].style.height = "0px";
+      icons[i].classList.remove(minusSign);
+      icons[i].classList.add(plusSign);
+      
+    }
+
+    for (let n = 0; j < contentDiv.length; j++) {
+      if (j !== i) {
+        contentDiv[n].style.height = "0px";
+        icons[n].classList.remove(minusSign);
+        icons[n].classList.add(plusSign);
+      }
+    }
+
+  });
+
+}
+
+/* image navigation code */
+
+const mainImage = document.querySelector(".main__image");
+const smallIcon = document.querySelector(".icon__small");
+
+mainImage.addEventListener("click", () => {
+  redirect("#credits");
+});
+
+smallIcon.addEventListener("click", () => {
+  redirect("#hero");
+})
---
+++ js/util.js
@@ -0,0 +1,92 @@
+function refreshPage() {
+  redirect(window.location.href);
+}
+
+/**
+ * @param {URL} redirectLocation
+ * @returns undefined
+ */
+
+function redirect(redirectLocation) {
+  window.location.href = redirectLocation;
+}
+
+/**
+ * @param {URL} redirectLocation
+ * @returns undefined
+ */
+
+function openNewPage(redirectLocation) {
+  parent.open(redirectLocation);
+}
+
+/**
+ * @param {string} message
+ * @param {string} type
+ */
+
+function notify(message, type, context) {
+  let creat;
+  if (
+    document.getElementById("notification-area") === undefined ||
+    document.getElementById("notification-area") === null
+  ) {
+    creat = document.createElement("div");
+    creat.id = "notification-area";
+
+    document.body.append(creat);
+  } else {
+    creat = document.getElementById("notification-area");
+  }
+
+  const preference = localStorage.getItem("notifications");
+
+  if (preference == "enabled") {
+    let createdDiv = document.createElement("div");
+    let id = Math.random().toString(36).substring(2, 10);
+
+    createdDiv.setAttribute("id", id);
+    createdDiv.classList.add("notification", type);
+
+    if (context) {
+      createdDiv.classList.add("contextMenu");
+    }
+
+    createdDiv.innerHTML = `
+<div class="cross">
+  <span class="cross__spans span1"></span>
+  <span class="cross__spans span2""></span>
+</div>
+${message}
+<span class="notif notif-timer"></span>`;
+    document.getElementById("notification-area").prepend(createdDiv);
+
+    setTimeout(() => {
+      let notifications = document
+        .getElementById("notification-area")
+        .getElementsByClassName("notification");
+      for (let i = 0; i < notifications.length; i++) {
+        if (notifications[i].getAttribute("id") == id) {
+          notifications[i].remove();
+          break;
+        }
+      }
+    }, 5000);
+    const notifCrosses = document.querySelectorAll(".notification .cross");
+    notifCrosses.forEach((e) => {
+      e.addEventListener("click", () => {
+        e.parentElement.remove();
+      });
+    });
+  } else {
+    return;
+  }
+}
+
+/**
+ * @param {*} text
+ */
+
+function copyText(text) {
+  navigator.clipboard.writeText(text);
+}
